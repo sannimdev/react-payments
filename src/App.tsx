004ecb6472e8fc628e3ff785da02293b3482492f
Hojoong Choi <inegg.apps@gmail.com>
2023-02-18 20:56:38 +0900

    feat: 라우턱 기본

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { CardDetail } from './pages/card-detail';
 import { CardEdit } from './pages/card-edit';
 import { CardList } from './pages/card-list';
+interface IRoutes {
+  [key: string]: JSX.Element;
+}
+
+const routes: IRoutes = {
+  '/card-detail': <CardDetail />,
+  '/card-edit': <CardEdit />,
+  '/card-add': <CardEdit />,
+  '/': <CardList />,
+};
 
 function App() {
-  return (
-    <>
-      <CardDetail />
-      <CardList />
-      <CardEdit />
-    </>
-  );
+  const { pathname } = window.location || '';
+
+  const path = '/' + pathname.substring(1).split('/')[0];
+
+  return routes[path] ? routes[path] : <CardList />;
 }
 
 export default App;
